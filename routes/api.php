397b18f863fadd14707acb19b6044ecff5afe26c
mahdi-nasirlo <72885979+mahdi-nasirlo@users.noreply.github.com>
2024-03-16 07:19:08 +0330

    Add:: auth url

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::prefix('/auth')
+    ->name('auth.')
+    ->group(function () {
+
+        Route::post("/login", [AuthController::class, 'Login']);
+        Route::post("/register", [AuthController::class, 'Register']);
+
+    });
